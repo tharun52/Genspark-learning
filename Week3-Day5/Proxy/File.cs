b68456e54d0630d00cf6e9f99ac3119b0271ae6d
unknown <thetharun52@gmail.com>
2025-05-23 15:03:19 +0530

    Design Patterns

---
+++ Week3-Day5/Proxy/File.cs
@@ -0,0 +1,79 @@
+using System;
+using System.IO;
+
+interface IFile
+{
+    void Read();
+}
+
+class File : IFile
+{
+    public string FileName { get; set; }
+
+    public File(string fileName)
+    {
+        FileName = fileName;
+    }
+
+    public void Read()
+    {
+        if (System.IO.File.Exists(FileName))
+        {
+            string content = System.IO.File.ReadAllText(FileName);
+            Console.WriteLine("[Access Granted] Reading sensitive file content...");
+            Console.WriteLine(content);
+        }
+        else
+        {
+            Console.WriteLine("No file found.");
+        }
+    }
+}
+
+class User
+{
+    public string Username { get; set; }
+    public string Role { get; set; }
+
+    public User(string username, string role)
+    {
+        Username = username;
+        Role = role;
+    }
+
+    public string GetRole()
+    {
+        return Role;
+    }
+}
+
+class ProxyFile : IFile
+{
+    private File realFile;
+    private User currentUser;
+    private string fileName;
+
+    public ProxyFile(string fileName, User user)
+    {
+        this.fileName = fileName;
+        realFile = new File(fileName);
+        currentUser = user;
+    }
+
+    public void Read()
+    {
+        string role = currentUser.GetRole();
+        if (role == "Admin")
+        {
+            realFile.Read();
+        }
+        else if (role == "User")
+        {
+            Console.WriteLine("[Limited Access] File Name: " + fileName);
+        }
+        else
+        {
+            Console.WriteLine("[Access Denied] You do not have permission to read this file.");
+        }
+    }
+}
---
+++ Week3-Day5/Proxy/Program.cs
@@ -0,0 +1,18 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        User admin = new User("Alice", "Admin");
+        User guest = new User("Bob", "Guest");
+        User user = new User("Charlie", "User");
+
+        IFile file1 = new ProxyFile("example.txt", admin);
+        file1.Read();
+
+        IFile file2 = new ProxyFile("example.txt", user);
+        file2.Read();
+
+        IFile file3 = new ProxyFile("example.txt", guest);
+        file3.Read();
+    }
+}
