95608d0a12915e34f0de20a7dffc7fa0bc1a2dba
tharun52 <thetharun52@gmail.com>
2025-07-01 10:45:04 +0530

    Docker

---
+++ Week9-Day2/SimpleApp/server.js
@@ -0,0 +1,23 @@
+const http = require('http');
+const fs = require('fs');
+const path = require('path');
+
+const hostname = '0.0.0.0';
+const port = 3000;
+
+const server = http.createServer((req, res) => {
+  const filePath = path.join(__dirname, 'index.html');
+  fs.readFile(filePath, (err, content) => {
+    if (err) {
+      res.writeHead(500);
+      res.end('Error loading file');
+    } else {
+      res.writeHead(200, {'Content-Type': 'text/html'});
+      res.end(content);
+    }
+  });
+});
+
+server.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`);
+});
